0bc303305287f964a1ca6173b60abc679b483c88
Darryl Dague <darryldague@Darryls-Air.attlocal.net>
2022-05-20 08:10:00 -0400

    add leetcode profile link

--- pages/resume.js
+++ pages/resume.js
@@ -33,7 +33,7 @@ const About = () => {
                     <h1 className="tracking-tighter font-extrabold text-2xl mb-2">Darryl Dague</h1>
                     <span className="relative z-0 inline-flex shadow-sm rounded-md">
                         <div className="relative">
-                            <button type="button" className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]" onClick={copyToClipboard.bind(null, 'me@darryldague.dev')}>Copy email</button>
+                            <button type="button" className="relative inline-flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]" onClick={copyToClipboard.bind(null, 'me@darryldague.dev')}>Copy email</button>
                             {copied &&
                                 <span className="w-max absolute left-1/2 -bottom-3/4 -translate-x-1/2 text-green-500 font-bold">&#10003; Copied</span>
                             }
@@ -42,13 +42,22 @@ const About = () => {
                             passHref
                             href='https://github.com/ddague1190'
                         >
-                        <button type="button" className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]">
+                        <button type="button" className="-ml-px relative inline-flex items-center p-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]">
 
                         <svg role="img" viewBox="0 0 24 24" height='20px' xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
                         </button>
                         </Link>
+                        <Link
+                            passHref
+                            href='https://leetcode.com/ddague/'
+                        >
+                        <button type="button" className="-ml-px relative inline-flex items-center p-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]">
+
+                        LeetCode Profile
+                        </button>
+                        </Link>
                         <Link href='./resume.pdf'>
-                        <button type="button" className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]">
+                        <button type="button" className="-ml-px relative inline-flex items-center p-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-[#43bdff] focus:border-[#43bdff]">
                             resume.pdf
                         </button>
                         </Link>
